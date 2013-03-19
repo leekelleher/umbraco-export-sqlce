de06fba9fff96a6f92c9278d81c80129d01640c7
leekelleher <leekelleher@gmail.com>
2013-03-19 12:49:09 +0000

    Tweaked MSBuild script to support generate Version number. (Upgraded to VS2012)

--- Our.Umbraco.Dashboard.ExportSqlCE/Properties/AssemblyInfo.cs
+++ Our.Umbraco.Dashboard.ExportSqlCE/Properties/AssemblyInfo.cs
@@ -2,8 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following set of attributes.
-// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("Our.Umbraco.Dashboard.ExportSqlCE")]
 [assembly: AssemblyDescription("Export SQL Server Compact")]
 [assembly: AssemblyConfiguration("")]
@@ -13,14 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible to COM components.
-// If you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6C82D377-90FE-4468-8C09-30732584C308")]
-
-// Version information for an assembly consists of the following four values:
-// [Major].[Minor].[Build].[Revision]
-[assembly: AssemblyVersion("2.0.0.*")]
-// [assembly: AssemblyFileVersion("2.0.0.*")]
